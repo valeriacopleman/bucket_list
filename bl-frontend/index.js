ca8000704fc9f88e15f4af25346daec3865d8fc1
Valeria Copleman <valeriacopleman@gmail.com>
2021-01-06 13:10:05 -0800

    created list on page

--- bl-frontend/index.js
+++ bl-frontend/index.js
@@ -6,9 +6,48 @@ window.addEventListener("DOMContentLoaded", () => {
 
 function getBuckets() {
     let main = document.getElementById('main')
+    main.innerHTML = ""
     fetch(BASE_URL + '/buckets')
     .then(res => res.json())
     .then(buckets => {
-        console.log(buckets)
+        buckets.map( bucket => {
+        
+        main.innerHTML += `
+        <li>
+        <a href="#" data-id="${bucket.id}">${bucket.name}</a>
+        </li>
+        ` 
+        }).join("")
+        attachClicksToLinks()
+    })
+
+}
+
+function attachClicksToLinks() {
+    let buckets = document.querySelectorAll("li a")
+    buckets.forEach(bucket => {
+        bucket.addEventListener('click', displayBucket)
     })
 }
+
+function displayBucket(e) {
+    console.log(e.target)
+    let id = e.target.dataset.id
+    main.innerHTML = ""
+    fetch(BASE_URL + `/buckets/${id}`)
+    .then(resp => resp.json())
+    .then(bucket => {
+        main.innerHTML = `
+        <h3>${bucket.name}:</h3> 
+        <br>
+        `
+        bucket.things.forEach( thing => {
+            main.innerHTML += `
+            <li>${thing.description}</li>
+            `
+        })
+    })
+}
+
+
+
